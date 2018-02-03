625eddfac496624e6ac5cd89d6d9a8b9e03e573f
Rishabh Sethi <sethi.rishabh97@gmail.com>
2018-02-04 01:38:55 +0530

    Create Longest Increasing Subsequence.cpp

---
+++ Longest Increasing Subsequence.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n+1];
+	    int dp[n+1];
+	    for(int i=1; i<=n; i++)
+	    {
+	        cin>>arr[i];
+	        dp[i] = 1;
+	    }
+	    for(int i=2; i<=n; i++)
+	    {
+	        for(int j=1; j<i; j++)
+	        {
+	            if(arr[i]>arr[j])
+	                dp[i] = max(dp[i],dp[j]+1);
+	        }
+	    }
+	    int ans = 0;
+	    for(int i=1; i<=n; i++)
+	        ans = max(ans,dp[i]);
+	    cout<<ans<<endl;
+	}
+	return 0;
+}
